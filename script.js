f360296f7a3a98981860c722e66336968d32db8e
CaioKwy <caiokaway@gmail.com>
2023-05-13 15:12:39 -0300

    posição do mouse e cores

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        cor: 'branco'
+        cor: 'branco',
+        posiçãoX: 0,
+        posiçãoY:0
     },
     methods: {
         mudarCor(c){
             this.cor = c
+
+        },
+        coordenadas(e){
+            this.posiçãoX = e.clientX
+            this.posiçãoY = e.clientY
 
         }
     }
